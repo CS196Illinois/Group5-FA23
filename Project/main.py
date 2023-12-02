33d8af1e995ee31b9ffc66aeea967a8d6d888723
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-12-02 15:58:53 -0600

    updated main

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,9 @@
 import tmdbsimple as tmdb
 from flask import Flask, render_template, request, redirect, url_for
 import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
 
 with open('Project/genres_movies.json') as f:
     genres_movies = json.load(f).get("genres_movies")
@@ -18,6 +21,22 @@ app = Flask(__name__)
 
 movies = []
 
+df = pd.read_csv('movie_dataset.csv').reset_index()
+
+features = ['keywords', 'cast', 'genres', 'director', 'overview', 'production_companies', 'tagline']
+for feature in features:
+    df[feature] = df[feature].fillna('')
+
+def combined_features(row):
+    return row['keywords'].replace('|', " ")+" "+row['cast'].replace('|', " ")+" "+row['genres'].replace('|', " ")+row['genres'].replace('|', " ")+row['genres'].replace('|', " ")+row['genres'].replace('|', " ")+" "+row['director']+" "+row['overview']+" "+row['production_companies'].replace('|', " ")+" "+row['tagline']
+df["combined_features"] = df.apply(combined_features, axis =1)
+
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(df["combined_features"])
+#print("Count Matrix:", count_matrix.toarray())
+
+cosine_sim = cosine_similarity(count_matrix)
+
 @app.route('/')
 def index():
     """
@@ -33,12 +52,35 @@ def add_task():
     search = tmdb.Search()
     movies.clear()
     movie = search.movie(query=request.form.get('movie'))
-    if movie:
-        for s in search.results:
-            if s['vote_average'] > 0.0:
-                genre_list = get_genres(s['genre_ids'])
-                if (len(genre_list)) > 0:
-                    movies.append(f"{s['title']} -- RATING: {str(s['vote_average'])} -- GENRES: {str(genre_list)}")
+    if (movie in df['original_title'].unique()) == False:
+        movies = ["Movie not found"];
+        return redirect(url_for('index'))
+        
+    index = df[df["original_title"] == movie]["index"].values[0]
+    
+    similar_movies = list(enumerate(cosine_sim[index]))
+    
+    movies = sorted(similar_movies, key=lambda x:x[1], reverse=True)
+    
+    sorted_by_popularity = []
+    
+    i = 0
+    
+    for movie in movies:
+        #print(get_title_from_index(movie[0]))
+        #print(sorted_similar_movies[i])
+        sorted_by_popularity.append({"TITLE":df[df.index == movie[0]]["original_title"].values[0]})
+        #print(df[df.original_title == "Jurassic World"]["popularity"])
+        i+=1
+        if i>50:
+            break
+        
+    # if movie:
+    #     for s in search.results:
+    #         if s['vote_average'] > 0.0:
+    #             genre_list = get_genres(s['genre_ids'])
+    #             if (len(genre_list)) > 0:
+    #                 movies.append(f"{s['title']} -- RATING: {str(s['vote_average'])} -- GENRES: {str(genre_list)}")
     return redirect(url_for('index'))
 
 def get_genres(genre_ids): 
